a3afdbe689a1e5346b4741dbc6e546bda697081e
fredoh91 <fredoh@gmail.com>
2025-05-11 14:26:29 +0200

    Ouverture des onglets "detail/eval" et "autre FU" en JS avec window.open()

--- assets/js/listeEvalSusar.js
+++ assets/js/listeEvalSusar.js
@@ -1,3 +1,54 @@
+console.log("Test Fredoh");
+
+
+// Pour ouvrir l'onglet détail/évaluation
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionne tous les boutons avec la classe "btn-open-detail"
+    const detailButtons = document.querySelectorAll('.btn-open-detail');
+
+    // Ajoute un gestionnaire d'événements à chaque bouton
+    detailButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Empêche le comportement par défaut du bouton
+
+            // Récupère l'URL à partir de l'attribut "data-url"
+            const url = button.getAttribute('data-url');
+
+            // Ouvre l'URL dans un nouvel onglet
+            if (url) {
+                window.open(url, '_blank');
+            } else {
+                console.error('URL non définie pour ce bouton.');
+            }
+        });
+    });
+});
+
+
+// Pour ouvrir l'onglet autre FU
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionne tous les boutons avec la classe "btn-open-autre-fu"
+    const detailButtons = document.querySelectorAll('.btn-open-autre-fu');
+
+    // Ajoute un gestionnaire d'événements à chaque bouton
+    detailButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Empêche le comportement par défaut du bouton
+
+            // Récupère l'URL à partir de l'attribut "data-url"
+            const url = button.getAttribute('data-url');
+
+            // Ouvre l'URL dans un nouvel onglet
+            if (url) {
+                window.open(url, '_blank');
+            } else {
+                console.error('URL non définie pour ce bouton.');
+            }
+        });
+    });
+});
+
+
 
 // // console.log("Test JS module : 1");
 
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ Encore
     .addEntry('listeEvalSusar', './assets/js/listeEvalSusar.js')
     .addEntry('intervenanSubstanceDetail', './assets/js/intervenantSubstanceDetail.js')
     .addEntry('SusarEuDetail', './assets/js/SusarEuDetail.js')
+
     // .addEntry('popUpAWA', './assets/js/popUpAWA.js')
 
     .addStyleEntry('type_eu_css', './assets/styles/type_eu.scss')
